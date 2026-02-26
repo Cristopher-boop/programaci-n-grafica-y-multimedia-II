b7ea3d1b0c7a61c2e42557b48e288d57638ebb01
Cristopher Diego Rodriguez Ayala <cristopherrodriguez1510@gmail.com>
2026-02-25 21:32:49 -0400

    Acabar practica 3

--- practica3/src/app/components/avatar/avatar.ts
+++ practica3/src/app/components/avatar/avatar.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-avatar',
@@ -9,23 +9,38 @@ import {CommonModule} from '@angular/common';
   styleUrl: './avatar.scss',
 })
 export class AvatarComponent {
- colorFondo = signal<string>('#3498db');
- tamano = signal<number>(150);
- tieneLentes = signal<boolean>(false);
- tipoExpresion = signal<number>(1);
+  colorFondo = signal<string>('#3498db');
+  tamano = signal<number>(150);
+  tieneLentes = signal<boolean>(false);
+  tipoExpresion = signal<number>(1);
+  etiquetaIdentidad = signal<string>('Pibe');
+  
+  modoRocola = signal<boolean>(false);
 
-  cambiarColor(event: Event){
+  cambiarColor(event: Event) {
     const input = event.target as HTMLInputElement;
     this.colorFondo.set(input.value);
   }
- cambiarTamano(event: Event){
-   const input = event.target as HTMLInputElement;
-   this.tamano.set(parseInt(input.value));
- }
- toggleLentes(){
-    this.tieneLentes.update(flag => !flag);
- }
- setExpresion(tipo: number){
+
+  cambiarTamano(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tamano.set(parseInt(input.value));
+  }
+
+  actualizarIdentidad(evento: Event) {
+    const elemento = evento.target as HTMLInputElement;
+    this.etiquetaIdentidad.set(elemento.value);
+  }
+
+  toggleLentes() {
+    this.tieneLentes.update(banderica => !banderica);
+  }
+  
+  toggleGiro() {
+    this.modoRocola.update(rotoncio => !rotoncio);
+  }
+
+  setExpresion(tipo: number) {
     this.tipoExpresion.set(tipo);
- }
+  }
 }
